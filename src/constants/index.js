1705a1ae8da78c1a69e36964d84c763dd9e21e0d
MaybeRosado <erosado@ucol.mx>
2024-11-13 20:16:08 -0600

    firts commit

---
+++ src/constants/index.js
@@ -0,0 +1,35 @@
+//import project1 from "../assets/projects/project-1.jpg";
+//import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+//import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I’m Emilio Rosado, a Software Engineering student at the University of Colima. I’m passionate about creating impactful solutions and constantly improving my skills in software development. Throughout my academic journey, I've worked on both academic and personal projects that have helped me build expertise in areas like web development (front-end and back-end) and databases. My main focus is to learn and adapt to new technologies to solve real-world problems. My goal is to build a career in the software industry, and I’m open to new opportunities to learn, collaborate, and grow professionally. Thank you for visiting my portfolio!`;
+
+export const ABOUT_TEXT = `I’m a student with a strong curiosity for technology and a drive to solve meaningful problems, I’m working towards becoming a well-rounded developer. My academic projects and self-driven learning have given me hands-on experience in various programming languages and frameworks, helping me build a solid foundation in both front-end and back-end development.
+As someone who values both technical skill and creativity, I enjoy exploring innovative ways to bring ideas to life. I believe that learning is a continuous journey, and I’m excited to keep expanding my expertise, especially in areas like database management and full-stack development. When I’m not coding, I enjoy reading, listening to music, watching movies, experimenting with new tools, and learning about emerging trends in tech.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - 2024",
+    role: "Frontend Developer",
+    company: "Fenix Apps.",
+    description: `Led to the frontend development of a demo for "Narices Frías," a pet cemetery company .`,
+    technologies: ["TypeScript", "React.js", "Next.js", "SASS"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "JavaScript", "Tailwind", "Framer Motion"],
+  }
+];
+
+export const CONTACT = {
+  address: "Villa de Álvarez, Colima",
+  phoneNo: "+52 3123199603 ",
+  email: "emilioaraujo15@gmail.com",
+};
